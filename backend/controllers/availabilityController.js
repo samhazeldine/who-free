2b4d15f55a2697ffc096487f1b21329b3e0119f5
Samuel Hazeldine <samhazeldine@hotmail.co.uk>
2021-03-18 22:30:48 +0000

    before mucking with proxy

--- backend/controllers/availabilityController.js
+++ backend/controllers/availabilityController.js
@@ -1,13 +1,5 @@
 const { availability:container } = require('../config').containers;
 
-const samItem = {
-    name: 'Sam',
-    dates : [
-      '01/02/2021',
-      '08/02/2021'
-    ]
-}
-
 async function getAvailability (req, res) {
     const querySpec = {
         query: "SELECT * from c"
@@ -26,12 +18,18 @@ async function getAvailability (req, res) {
     }
 }
 
-async function createAvailability (req, res) {
-    console.log("post");
-    const { item } = await container
+async function createAvailability (req, res) {    
+    try {
+        const {item} = await container
         .items
-        .upsert(samItem);
-    console.log(`Created Sam item with ID` + item.id);
+        .upsert(req.body);
+
+        res.status(200).json(item.id);
+    }
+    catch(err) {
+        res.status(400).send(err.message);
+    }
+    
 }
 
 module.exports = {getAvailability, createAvailability};
